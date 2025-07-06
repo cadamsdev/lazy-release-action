e54441d4891495d0bf0475f72e42a9ca5aacdfc9
Chad Adams <cadamsdev@outlook.com>
2025-07-06 12:57:20 -0400

    feat: can now publish packages to npm (#22)

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,4 @@ export const SNAPSHOTS_ENABLED = process.env['INPUT_SNAPSHOTS']
   ? process.env['INPUT_SNAPSHOTS'] === 'true'
   : false;
 export const DEFAULT_BRANCH = process.env['INPUT_DEFAULT-BRANCH'] || 'main';
+export const NPM_TOKEN = process.env['INPUT_NPM-TOKEN'] || '';
