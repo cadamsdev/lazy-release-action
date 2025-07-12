0800d6b3fa3e5530584624bd057a140c95c031e4
cadamsdev <cadamsdev@outlook.com>
2025-07-12 12:14:56 -0400

    update commit pattern logic

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,6 +1,6 @@
 
 export const CONVENTIONAL_COMMITS_PATTERN =
-  /^(feat|fix|perf|chore|docs|style|test|build|ci|revert)(!)?(\(([a-z-0-9]+)(,\s*[a-z-0-9]+)*\))?(!)?: .+/; // https://regexr.com/8flmk
+  /^(feat|fix|perf|chore|docs|style|test|build|ci|revert)(!)?(\(([a-z-0-9]+)(,\s*[a-z-0-9]+)*\))?(!)?(#(major|minor|patch))?: .+/; // https://regexr.com/8g0dc
 
 
 export function isPRTitleValid(prTitle: string): boolean {
